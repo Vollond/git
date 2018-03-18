711332bb1d68c49cc694e29171aa3b2cc49fa85b
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-18 13:25:33 +0200

    1

--- index.php
+++ index.php
@@ -681,11 +681,11 @@ $bot->on(function($Update) use ($bot){
 	//$message->getFrom()->getId() 406900318
 	}
 	if($uname!="nitcshe"){
-	if($cid==-1001132568494){
-	if($message->getChat()->getUsername() == "PProgProm"){
-	$bot->deleteMessage(-1001132568494, $message->getMessageId());	
+	if($cid==-1001319320245){
+	if($message->getChat()->getUsername() == "testqq1"){
+	$bot->deleteMessage(-1001319320245, $message->getMessageId());	
 	$bot->sendMessage("322682583", "$uname : $mtext");		
-	$bot->restrictChatMember(-1001132568494, $uid , strtotime("+3660 days"), false, false, false, false);
+	$bot->restrictChatMember(-1001319320245, $uid , strtotime("+3660 days"), false, false, false, false);
 	}}}
 	
 	
