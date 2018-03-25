945d8b14baab500d0e8512ac3f40ed6d16a99467
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-25 16:14:33 +0300

    1

--- index.php
+++ index.php
@@ -700,6 +700,16 @@ $bot->on(function($Update) use ($bot){
 	$bot->restrictChatMember(-1001394826177, $uid , strtotime("+3660 days"), false, false, false, false);
 	}}}
 	
+			if(($uname!="nitcshe")&&($uname!="upRiseup")){ 
+	if($cid==-1001338957812){
+	if($message->getChat()->getUsername() == "igatget0"){
+	$bot->deleteMessage(-1001338957812, $message->getMessageId());	
+	//$bot->sendMessage("322682583", "$uname : $mtext");
+	$txt="$uname :  $mtext";
+	logg($txt);
+	$bot->restrictChatMember(-1001338957812, $uid , strtotime("+3660 days"), false, false, false, false);
+	}}}
+	
 	
 	if(mb_stripos($mtext,"Сиськи") !== false){
 		$pic = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961.jpg";
