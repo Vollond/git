7e6083a81a524a8efd08ff96650fe9aea5c62a78
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-30 20:41:00 +0300

    1

--- index.php
+++ index.php
@@ -720,6 +720,17 @@ $bot->on(function($Update) use ($bot){
 	$bot->restrictChatMember(-1001198436799, $uid , strtotime("+3660 days"), false, false, false, false);
 	}}}
 	
+				if(($uname!="honeymoneyadm")&&($uname!="Manager_Advanceup")){ 
+	if($cid==-1001235118466){
+	if($message->getChat()->getUsername() == "hmdep"){
+	$bot->deleteMessage(-1001235118466, $message->getMessageId());	
+	//$bot->sendMessage("322682583", "$uname : $mtext");
+	$txt="$uname :  $mtext";
+	logg($txt);
+	$bot->restrictChatMember(-1001235118466, $uid , strtotime("+3660 days"), false, false, false, false);
+	}}}
+	
+	
 	
 	if(mb_stripos($mtext,"Сиськи") !== false){
 		$pic = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961.jpg";
