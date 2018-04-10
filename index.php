1438a18e91b1cd82efa0b8bf23f9373d7fa8e739
Vollond <34167687+Vollond@users.noreply.github.com>
2018-04-10 18:24:02 +0300

    1

--- index.php
+++ index.php
@@ -759,6 +759,16 @@ $bot->on(function($Update) use ($bot){
 	logg($txt);
 	$bot->restrictChatMember(-1001169044431, $uid , strtotime("+3660 days"), false, false, false, false);
 	}}}
+	
+							if(($uname!="Vlas0v")&&($uname!="Manager_Advanceup")){ 
+	if($cid==-1001255743792){
+	if($message->getChat()->getUsername() == "restotop2"){
+	$bot->deleteMessage(-1001255743792, $message->getMessageId());	
+	//$bot->sendMessage("322682583", "$uname : $mtext");
+	$txt="$uname :  $mtext";
+	logg($txt);
+	$bot->restrictChatMember(-1001255743792, $uid , strtotime("+3660 days"), false, false, false, false);
+	}}}
 
 	if(mb_stripos($mtext,"Сиськи") !== false){
 		$pic = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961.jpg";
